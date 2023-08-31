b93ae74067b66752553039402fbc8e75ffedadf6
Nick Bechtel <bechtel.nick@gmail.com>
2023-08-31 12:38:32 -0400

    updated to header and footer

--- app/footer/footer.js
+++ app/footer/footer.js
@@ -1,5 +1,28 @@
 function footer() {
-  return <div>footer</div>;
+  return (
+    <div className="h-[600px] w-screen fixed inset-x-0 bottom-0 bg-black text-white flex justify-center p-[80px]">
+      <div>
+        <div className="w-full flex justify-between">
+          <div>
+            <div>Join our Newsletter</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div>
+            <div>
+              <input type="text" placeholder="Email Address" />
+              <button>Subscribe</button>
+            </div>
+            <p>
+              By subscribing you agree to with our Privacy Policy and provide
+              consent to receive updates from our company.
+            </p>
+          </div>
+        </div>
+        <div>middle</div>
+        <div>bottom</div>
+      </div>
+    </div>
+  );
 }
 
 export default footer;
--- app/header/header.js
+++ app/header/header.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 function header() {
@@ -10,12 +11,16 @@ function header() {
   ];
   return (
     <div className="w-screen h-[100px] relative">
-      <div className="flex flex-row items-center justify-between px-10 py-6">
-        <div className="">logo</div>
+      <div className="flex flex-row items-center justify-around px-10 py-6">
+        <div className="">
+          <Link href={"/"}>
+            <Image src={"/CAI.png"} width={125} height={125} />
+          </Link>
+        </div>
         <div className="">
           <ul className="flex flex-row items-center">
             {navItems.map((item) => (
-              <li className="px-4">
+              <li className="px-4 text-m font-bold">
                 <Link href={item.url}>{item.label}</Link>
               </li>
             ))}
