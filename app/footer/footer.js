1e5b43f0552bbcd1211459aa3c5ce0b73582a961
Nick Bechtel <bechtel.nick@gmail.com>
2023-08-31 14:43:59 -0400

    updates to input styling

--- app/footer/footer.js
+++ app/footer/footer.js
@@ -10,7 +10,7 @@ function footer() {
   return (
     <div className="h-[600px] w-screen fixed inset-x-0 bottom-0 bg-black text-white flex justify-center ">
       <div className="p-[80px] w-full">
-        <div className="w-full flex justify-between py-6">
+        <div className="w-full flex justify-between py-6 pb-20">
           <div>
             <div className="font-bold text-2xl">Join our Newsletter</div>
             <p className="text-base font-light">
@@ -22,7 +22,7 @@ function footer() {
               <input
                 type="text"
                 placeholder="Enter your Email"
-                className=" border-solid border-2  border-[#FCE458] bg-black/50  text-white/50 rounded-full focus:outline-none w-[80%] h-[48px] px-4"
+                className=" border-solid border-2  border-[#FCE458] bg-black/50  text-white/80 rounded-full focus:outline-none w-[80%] h-[48px] px-4 placeholder:text-white/80"
               />
               <button className="border bg-[#FCE458] rounded-full text-black font-bold px-4 py-2 hover:bg-[#ffeb77] hover:text-[#454545]">
                 Subscribe
@@ -34,7 +34,7 @@ function footer() {
             </p>
           </div>
         </div>
-        <hr className="py-4" />
+        <hr className="py-4 pb-20" />
         <div className="py-6">
           <div className="flex flex-row justify-between items-center">
             <div>
